ae5d6c5a54e5cae1c91549a2bda24bf9f43967a0
yougang prosper <127027386+yougang237@users.noreply.github.com>
2023-03-07 14:02:08 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# prompt for user input
+
+a = float(input("enter the first number: "))
+b = float(input("enter the second number: "))
+
+# perfom operations
+
+print("sum: {} + {} = {}".format(a,b,a+b))
+print("difference: {} - {} = {}".format(a,b,a-b))
+print("quotient: {} / {} = {}".format(a,b,a/b))      
+print("power: {} ^ {} = {}".format(a,b,a**b))
+print("division with remainder: {} / {} = {} remainder: {}".format(a,b,a+b))
+
+
